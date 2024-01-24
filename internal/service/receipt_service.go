c76904239bc1f790adfbd939174aaef6bf337612
vineethkanaparthi <vineeth0025@gmail.com>
2024-01-24 13:43:08 -0700

    refactoring

--- internal/service/receipt_service.go
+++ internal/service/receipt_service.go
@@ -39,53 +39,61 @@ func calculatePoints(receipt *model.Receipt) int {
 
 	// Rule 1: One point for every alphanumeric character in the retailer name
 	points += countAlphanumericCharacters(receipt.Retailer)
-	fmt.Printf("Rule 1: %d\n", points)
-	// Rule 2: 50 points if the total is a round dollar amount with no cents
-	if cents := receipt.Total[len(receipt.Total)-2:]; cents == "00" {
-		points += 50
-	}
-	fmt.Printf("Rule 2: %d\n", points)
+	fmt.Printf("Points after Rule 1: %d\n", points)
 
-	// Rule 3: 25 points if the total is a multiple of 0.25
+	// Rule 2: 50 points if the total is a round dollar amount with no cents
 	totalFloat := 0.0
 	fmt.Sscanf(receipt.Total, "%f", &totalFloat)
+	if totalFloat == float64(int(totalFloat)) {
+		points += 50
+	}
 	fmt.Println(totalFloat)
+	fmt.Printf("Points after Rule 2: %d\n", points)
+
+	// Rule 3: 25 points if the total is a multiple of 0.25
 	if totalFloat/0.25 == float64(int(totalFloat/0.25)) {
 		points += 25
 	}
-	fmt.Printf("Rule 3: %d\n", points)
+	fmt.Printf("Points after Rule 3: %d\n", points)
 
 	// Rule 4: 5 points for every two items on the receipt
 	points += 5 * (len(receipt.Items) / 2)
-	fmt.Printf("Rule 4: %d\n", points)
+	fmt.Printf("len of items: %d\n", len(receipt.Items))
+	fmt.Printf("Points after Rule 4: %d\n", points)
 
 	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2
 	// and round up to the nearest integer. The result is the number of points earned.
 	for _, item := range receipt.Items {
 		trimmedLength := len(strings.Trim(item.ShortDescription, " "))
+		fmt.Printf("item trimmed: %s, length: %d\n", strings.Trim(item.ShortDescription, " "), trimmedLength)
 		if trimmedLength%3 == 0 {
 			priceFloat, _ := strconv.ParseFloat(item.Price, 64)
 			points += int(math.Ceil(priceFloat * 0.2))
 		}
 	}
-	fmt.Printf("Rule 5: %d\n", points)
+	fmt.Printf("Points after Rule 5: %d\n", points)
 
 	// Rule 6: 6 points if the day in the purchase date is odd
-	purchaseDay, _ := strconv.Atoi(receipt.PurchaseDate[len(receipt.PurchaseDate)-2:])
-	fmt.Println(purchaseDay)
-	if purchaseDay%2 != 0 {
-		points += 6
+	if receipt.purchaseDate != "" {
+		purchaseDate, _ := time.Parse("2006-01-02", receipt.PurchaseDate)
+		purchaseDay := purchaseDate.Day()
+		fmt.Println(purchaseDay)
+		if purchaseDay%2 != 0 {
+			points += 6
+		}
 	}
-	fmt.Printf("Rule 6: %d\n", points)
+	fmt.Printf("Points after Rule 6: %d\n", points)
 
 	// Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm
-	purchaseTime, _ := time.Parse("15:04", receipt.PurchaseTime)
-	fmt.Printf("%+v\n", purchaseTime)
-	if purchaseTime.After(time.Date(0, 1, 1, 14, 0, 0, 0, time.UTC)) &&
-		purchaseTime.Before(time.Date(0, 1, 1, 16, 0, 0, 0, time.UTC)) {
-		points += 10
+	if receipt.purchaseTime != "" {
+		purchaseTime, _ := time.Parse("15:04", receipt.PurchaseTime)
+		fmt.Printf("%+v\n", purchaseTime)
+		if purchaseTime.After(time.Date(0, 1, 1, 14, 0, 0, 0, time.UTC)) &&
+			purchaseTime.Before(time.Date(0, 1, 1, 16, 0, 0, 0, time.UTC)) {
+			points += 10
+		}
 	}
-	fmt.Printf("Rule 7: %d\n", points)
+	fmt.Printf("Points after Rule 7: %d\n", points)
 
 	return points
 }
